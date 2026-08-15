ff8563dcdef69e571dc299585f826934816ea730
Lazu <bizcontactneeded@gmail.com>
2026-08-15 23:22:04 +0200

    addTask and listTask commands completed, main function probably fixed, atributeError needs fixing

--- tasker.py
+++ tasker.py
@@ -1,56 +1,73 @@
+import string
 import sys
 import json
 import argparse
 
 TASKS_FILE = "tasks.json"
 
-def listTask():
-    with open(TASKS_FILE,"r") as jsonFile:
-        loadFile=json.load(jsonFile)
-        for x in loadFile:
-            print(x,loadFile[x])
+def listTask(_args):
+    try:
+        with open(TASKS_FILE,"r") as jsonFile:
+            tasks=json.load(jsonFile)
 
-def removeTask(key):
+    except (FileNotFoundError, json.JSONDecodeError):
+        tasks = []
+
+    for number, task in enumerate(tasks,start = 1):
+        print(number, task)
+
+def addTask(args):#args.task
+    try:
+        with open(TASKS_FILE,"r") as jsonFile:
+            tasks = json.load(jsonFile)
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        tasks = []
+
+    tasks.append(args.task)
+
+    with open(TASKS_FILE, "w") as jsonFile:
+        json.dump(tasks,jsonFile,indent=4)
+        
+        
+        
+
+def removeTask(args):
     with open(TASKS_FILE,"w") as jsonFile:
         loadFile=json.load(jsonFile)
-        loadFile[key] = ""
+        loadFile[args] = ""
 
 def main():
     parser = argparse.ArgumentParser(prog="tasker", description="tasker is a task tracker and manager which helps you with organising your tasks",epilog="temp nothing")
-    #parser.add_argument("list",help="prints all tasks from tasks.json")
     subparsers = parser.add_subparsers(
+        title="commands",
         dest="command",
-        required=True
+        required=False
         )
-    
-    list_parser = subparsers.add_parser(
+
+    listParser = subparsers.add_parser(
         "list",
         help="prints all tasks from tasks.json",
         aliases = ["ls"]
         )
+    listParser.set_defaults(func=listTask)
 
-    remove_parser = subparsers.add_parser(
+    addParser = subparsers.add_parser(
+        "add",
+        help="used to create new tasks",
+        )
+    addParser.add_argument("task",type=str)
+    addParser.set_defaults(func=addTask)
+
+    
+
+    removeParser = subparsers.add_parser(
         "remove",
         help="removes task by key"
         ) 
-
-    list_parser.set_defaults(func=listTask) 
+     
     args = parser.parse_args()
-    args.func()
-
-    
-
-    #if len(sys.argv) < 2:
-    #    print("Usage: python program.py <name>")
-    #    listTasks()
-    #    sys.exit(1)
-    
-    #name = sys.argv[1]
-    
-
-    #print(f"Hello, {name}!")
-
-    #print(f"Arguments: {sys.argv}")
+    args.func(args)
 
 if __name__ == "__main__":
     main()
